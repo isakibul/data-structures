014446874ad4d1ed19ead4a6aa591c744f3f9b84
isakibul <isakibul108@gmail.com>
2025-01-25 00:01:25 +0600

    bubble sort

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,20 +1,10 @@
 #include <stdio.h>
 
-// perform the bubble sort
 void bubbleSort(int array[], int size) {
-
-  // loop to access each array element
   for (int step = 0; step < size - 1; ++step) {
       
-    // loop to compare array elements
     for (int i = 0; i < size - step - 1; ++i) {
-      
-      // compare two adjacent elements
-      // change > to < to sort in descending order
       if (array[i] > array[i + 1]) {
-        
-        // swapping occurs if elements
-        // are not in the intended order
         int temp = array[i];
         array[i] = array[i + 1];
         array[i + 1] = temp;
@@ -23,7 +13,6 @@ void bubbleSort(int array[], int size) {
   }
 }
 
-// print array
 void printArray(int array[], int size) {
   for (int i = 0; i < size; ++i) {
     printf("%d  ", array[i]);
@@ -33,8 +22,6 @@ void printArray(int array[], int size) {
 
 int main() {
   int data[] = {-2, 45, 0, 11, -9};
-  
-  // find the array's length
   int size = sizeof(data) / sizeof(data[0]);
 
   bubbleSort(data, size);
